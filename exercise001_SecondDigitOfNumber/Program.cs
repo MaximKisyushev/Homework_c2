4b808212c39cd79c5b8451be28829df54d9608b4
MaximKisyushev <kisyushevmax@yandex.ru>
2022-08-13 21:43:14 +0500

    add exercise 001 Second digit of number

---
+++ exercise001_SecondDigitOfNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿// **Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = (number / 10) % 10;
+
+if (number / 100 > 0 && number / 100 <= 9) Console.WriteLine($"{number} -> {secondDigit}");
+else Console.WriteLine("Введено не трёхзначное число");
