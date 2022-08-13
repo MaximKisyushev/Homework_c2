848fcce64e3683fa70e925064e353d2a6597d467
MaximKisyushev <kisyushevmax@yandex.ru>
2022-08-14 00:17:16 +0500

    add exercise 003 Day Off

---
+++ exercise003_DayOff/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число обозначающее день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber == 6 || dayNumber == 7) Console.WriteLine($"{dayNumber} день недели - выходной");
+else Console.WriteLine($"{dayNumber} день недели - НЕ выходной");
